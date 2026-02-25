451d81b36c524c530ef8120aa8325b635ec977f6
Artem Kutsan <artemkutsan@gmail.com>
2026-02-25 16:49:27 +0100

    add: Добавление валидации заголовка и текста поста

--- src/components/CreatePost/index.jsx
+++ src/components/CreatePost/index.jsx
@@ -3,26 +3,54 @@ import styles from './CreatePost.module.css';
 import { useForm } from 'react-hook-form';
 
 function CreatePost({ onCreate }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors, dirtyFields },
+  } = useForm();
+
+  const postTitle = watch('title');
+  const postText = watch('text');
+
+  const onSubmit = (data) => {
+    onCreate({ ...data, createdAt: Date.now() });
+    reset();
+  };
 
   return (
     <div className={styles.wrapper}>
       <h3>Создать пост</h3>
 
-      <form
-        onSubmit={handleSubmit((data) => onCreate({ ...data, createdAt: Date.now() }))}
-        className={styles.form}
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <input
           type="text"
           placeholder="Заголовок"
           className={styles.input}
-          {...register('title')}
+          {...register('title', {
+            required: 'Введите заголовок',
+          })}
         />
+        {!postTitle && dirtyFields.title && (
+          <span style={{ color: 'red', fontSize: '14px' }}> Заголовок не может быть пустым </span>
+        )}
+        <textarea
+          placeholder="Текст поста"
+          className={styles.textarea}
+          {...register('text', {
+            required: 'Введите текст поста',
+          })}
+        />
+        {!postText && dirtyFields.text && (
+          <span style={{ color: 'red', fontSize: '14px' }}> Пост не может быть пустым </span>
+        )}
 
-        <textarea placeholder="Текст поста" className={styles.textarea} {...register('text')} />
-
-        <button type="submit" className={styles.create}>
+        <button
+          disabled={!postTitle || !postText || errors.title || errors.text}
+          type="submit"
+          className={styles.create}
+        >
           Создать
         </button>
       </form>
