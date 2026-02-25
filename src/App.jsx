5c7f9533d387f29d4848148c9ef90cec9d72819e
Artem Kutsan <artemkutsan@gmail.com>
2026-02-25 09:50:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="container">React Practice 10</h1>
+
+      <section>
+        <div className="container">
+          <h2>Задача 1: Блог</h2>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
