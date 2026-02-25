7de1c87e31a456690ded775b3c061c03d3846927
Artem Kutsan <artemkutsan@gmail.com>
2026-02-25 10:21:44 +0100

    update: Базовая структура Header

--- src/components/sections/Header/index.jsx
+++ src/components/sections/Header/index.jsx
@@ -3,8 +3,19 @@
 
 function Header() {
   return (
-    <header>
-      <div className="container">Компонент Header</div>
+    <header style={{ borderBottom: '1px solid #d4d4d4' }}>
+      <div className="container" style={{ display: 'flex', gap: '2rem', alignItems: 'center' }}>
+        <h2>MECK</h2>
+        <nav style={{ display: 'flex', gap: '2rem', alignItems: 'center' }}>
+          <a href="/">Главная</a>
+          <span>|</span>
+          <a href="/">Музыка</a>
+          <span>|</span>
+          <a href="/">Сообщества</a>
+          <span>|</span>
+          <a href="/">Друзья</a>
+        </nav>
+      </div>
     </header>
   );
 }
